b3660a16e873ffe1ff834a7bd2ec9740a9296197
ngc6611v <vgargueta@gmail.com>
2022-01-16 14:45:26 -0600

    Configurar webpack con React – Parte 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-//aquí utilizaremos módulos en JS
-//es reutilizar el código de JS en diferentes archivos
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import logo from "./background.jpg"
 
-import "./js/app";
+const container = document.getElementById("root");
+const App = () => (<div className="app">
+    <h1>V2 Hola React :)</h1>
+    <img src = {logo} />
+</div>);
 
+ReactDOM.render(<App />, container);
